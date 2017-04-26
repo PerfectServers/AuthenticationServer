237ef5aaf2cdb39b1aab98329e193f2c64f118da
Jonathan Guthrie <jono@perfect.org>
2017-04-26 11:06:36 -0400

    Finishing JSON routes

--- Sources/OAuth2-Server/Routing/Routes.swift
+++ Sources/OAuth2-Server/Routing/Routes.swift
@@ -11,6 +11,8 @@ import PerfectHTTPServer
 func mainRoutes() -> [[String: Any]] {
 
 	var routes: [[String: Any]] = [[String: Any]]()
+
+	// WEB
 	routes.append(["method":"get", "uri":"/", "handler":WebHandlers.main])
 	routes.append(["method":"post", "uri":"/login", "handler":WebHandlers.login])
 	routes.append(["method":"get", "uri":"/logout", "handler":WebHandlers.logout])
@@ -19,6 +21,14 @@ func mainRoutes() -> [[String: Any]] {
 	routes.append(["method":"post", "uri":"/register", "handler":WebHandlers.registerPost])
 	routes.append(["method":"get", "uri":"/verifyAccount/{passvalidation}", "handler":WebHandlers.registerVerify])
 	routes.append(["method":"post", "uri":"/registrationCompletion", "handler":WebHandlers.registerCompletion])
+
+	// JSON
+	routes.append(["method":"get", "uri":"/api/v1/session", "handler":JSONHandlers.session])
+	routes.append(["method":"get", "uri":"/api/v1/logout", "handler":JSONHandlers.logout])
+	routes.append(["method":"post", "uri":"/api/v1/register", "handler":JSONHandlers.register])
+	routes.append(["method":"login", "uri":"/api/v1/login", "handler":JSONHandlers.login])
+
+
 
 	routes.append(["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
 	               "documentRoot":"./webroot",
